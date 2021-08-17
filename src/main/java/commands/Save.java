63684c1a21e3f25755d34804911c7f64763d0c95
Nhi Nguyen <ayukiie@gmail.com>
2021-08-16 22:31:24 -0400

    fix typos

--- src/main/java/commands/Save.java
+++ src/main/java/commands/Save.java
@@ -18,7 +18,7 @@ public class Save extends Command {
 
     /**
      * This method will save the current JShell session into a file which will
-     * created/ over written at the given path
+     * created overwritten at the given path
      * @param shellState is the current state of JShell program
      * @param arguments  list of arguments that were passed along with command
      * @throws Exception if any of the paths is invalid
@@ -28,21 +28,21 @@ public class Save extends Command {
         try {
             checkArgumentsNum(arguments);
             String path = arguments.get(0);
-            if (path.indexOf("/") == -1) {
+            if (!path.contains("/")) {
                 throw new Exception(Exceptions.WRONG_PATH_INPUT_MSG);
             }
             FileWriter fileWriter = new FileWriter(path);
             BufferedWriter bw = new BufferedWriter(fileWriter);
             //add history
-            for (String commadLine : shellState.getHistory()) {
-                bw.write(commadLine);
+            for (String commandLine : shellState.getHistory()) {
+                bw.write(commandLine);
                 bw.newLine();
             }
             bw.write("*****");
             bw.newLine();
             // add content to file
-            for (String commadLine : shellState.getCorrectHistory()) {
-                bw.write(commadLine);
+            for (String commandLine : shellState.getCorrectHistory()) {
+                bw.write(commandLine);
                 bw.newLine();
             }
             // close bufferedWriter
