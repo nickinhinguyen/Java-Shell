eded52f1029f341a52d3a80e65ed57821a015f09
Nhi Nguyen <ayukiie@gmail.com>
2021-09-16 18:31:08 -0400

    change load from reading history file to deserialize existing JShellState

--- src/main/java/commands/Load.java
+++ src/main/java/commands/Load.java
@@ -1,10 +1,5 @@
 package commands;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import constants.Exceptions;
 import driver.IShellState;
@@ -24,65 +19,24 @@ public class Load extends Command{
      * @param arguments  list  of arguments that were passed along with command
      * @throws Exception if any of the paths is invalid
      */
-    public String executeCommand(IShellState shellState, List<String> arguments)
-            throws Exception {
-
-        try {
-            checkArgumentsNum(arguments);
-            // check if load was the first to be called in new session
-            List<String> his = shellState.getHistory();
-            if (his.size() != 1) {
-                throw new Exception("Load can only be called at the start of a session");
-            }
-            String path = arguments.get(0);
-            if (path.indexOf("/") == -1) {
-                throw new Exception(Exceptions.WRONG_PATH_INPUT_MSG);
-            }
-            String line;
-
-            FileReader fileReader = new FileReader(path);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            CommandExecutor commandExecutor = new CommandExecutor();
-            boolean nextPart = false;
-            int count = 0;
-            String currentHistory = shellState.getHistory().get(0);
-            shellState.removeHistory(0);
-
-            // load the file system
-            while((line = bufferedReader.readLine()) != null) {
-                if (line.equals("*****")) {
-                    nextPart = true;
-                }
-                else if (!nextPart) {
-                    shellState.addHistory(line);
-
-                }
-                else{
-                    // executeCommand correct command
-                    count ++;
-                    commandExecutor.executeCommand(shellState, line);
-                }
-
-            }
-
-            bufferedReader.close();
-            // remove double history
-            for (int i = 0; i<count; i++) {
-                int currentSize = shellState.getHistory().size();
-                shellState.removeHistory(currentSize-1);
-            }
-            shellState.addHistory("exit");
-            shellState.addHistory(currentHistory);
-            return "";
+    public String executeCommand(IShellState shellState, List<String> arguments) throws Exception {
+        checkArgumentsNum(arguments);
+        // check if load was the first to be called in new session
+        List<String> his = shellState.getHistory();
+        if (his.size() != 1) {
+            throw new Exception("Load can only be called at the start of a session");
         }
-        catch (FileNotFoundException e) {
-            throw new Exception(Exceptions.WRONG_PATH_INPUT_MSG);
-        }
-        catch(IOException ex) {
+        String path = arguments.get(0);
+        if (path.indexOf("/") == -1) {
             throw new Exception(Exceptions.WRONG_PATH_INPUT_MSG);
         }
 
+        loadFromRepository(shellState.getFileSystem(), shellState, path);
+        return "";
+    }
+
+    public void loadFromRepository(DataRepositoryInterface repo, IShellState shellState, String path ) throws Exception {
+        repo.loadJShell(shellState, path);
     }
 
 }
